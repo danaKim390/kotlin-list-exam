a6f5dbe11efdc24d35eda27518fe565005910d22
hyejin <>
2018-02-14 22:15:45 +0900

    use the hashMap

---
+++ app/doc/01_basic_syntax.md
@@ -0,0 +1,23 @@
+# 코틀린 기본 문법
+
+## 궁금한 키워드
+### internal
+
+
+### layout key가져올때 find할 필요 없이 extention으로 바로 가져온다 
+
+### HashMap 
+- Java
+```java
+HashMap<Sting, Value> map = new HashMap<>;
+map.put ...
+```
+- Kotlin
+```kotlin
+ mapOf() 
+```
+
+
+
+
+### get, set 줄여서 사용 
--- app/src/main/java/dana/com/kotlinlistexam/MainActivity.kt
+++ app/src/main/java/dana/com/kotlinlistexam/MainActivity.kt
@@ -2,11 +2,20 @@ package dana.com.kotlinlistexam
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.IntegerRes
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
+    val TAG = "MainActivity";
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val readOnlyMap = mapOf<String, Int>("A" to 1, "B" to 2, "C" to 3)
+
+        Log.d(TAG, readOnlyMap.toString())
+        Log.d(TAG, readOnlyMap.get("A").toString())
+        Log.d(TAG, readOnlyMap["A"].toString())
     }
 }
